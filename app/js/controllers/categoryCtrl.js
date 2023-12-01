eeca9cc66f56a7144a63572f94fb7137e3febc3d
thumb-dev <kevin@thumbprint.com>
2023-12-01 14:19:45 -0500

    Update categoryCtrl.js

--- app/js/controllers/categoryCtrl.js
+++ app/js/controllers/categoryCtrl.js
@@ -1,5 +1,5 @@
-four51.app.controller('CategoryCtrl', ['$routeParams', '$sce', '$scope', '$451', 'Category', 'Product', 'Nav',
-function ($routeParams, $sce, $scope, $451, Category, Product, Nav) {
+four51.app.controller('CategoryCtrl', ['$routeParams', '$sce', '$scope', '$451', 'Category', 'Product', 'Nav', '$modal',
+function ($routeParams, $sce, $scope, $451, Category, Product, Nav, $modal) {
 	$scope.productLoadingIndicator = true;
 	$scope.settings = {
 		currentPage: 1,
@@ -18,6 +18,36 @@ function ($routeParams, $sce, $scope, $451, Category, Product, Nav) {
 			$scope.searchLoading = false;
 		}, $scope.settings.currentPage, $scope.settings.pageSize);
 	}
+	
+    $scope.animationsEnabled = true;
+
+    $scope.openProductModal = function (li) {
+        console.log('in function');
+		var type = 'normal order';
+        var modalInstance = $modal.open({
+            animation: $scope.animationsEnabled,
+            templateUrl: 'productModal.html',
+            controller: 'productModalCtrl',
+            backdrop: 'static',
+            resolve: {
+                lineitem: function () {
+                    return li;
+                },
+                user: function () {
+                    return $scope.user;
+                },
+                currentOrder: function () {
+                    return $scope.currentOrder;
+                },
+				display: function() {
+					return type
+				}
+            }
+        });
+        modalInstance.result.then(function(o){
+            $scope.currentOrder = o;
+        });
+	};
 
 	$scope.$watch('settings.currentPage', function(n, o) {
 		if (n != o || (n == 1 && o == 1))
@@ -45,7 +75,7 @@ function ($routeParams, $sce, $scope, $451, Category, Product, Nav) {
     // panel-nav
     $scope.navStatus = Nav.status;
 	//default the category panel to be collapsed
-	$scope.navStatus.visible = false;
+	$scope.navStatus.visible = true;
 
     $scope.toggleNav = Nav.toggle;
 	$scope.$watch('sort', function(s) {
